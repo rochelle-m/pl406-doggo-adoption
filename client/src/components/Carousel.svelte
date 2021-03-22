7a7bc633d4d88a38a79cb755eb785c2b759eab32
chetana-1929 <mca.1929@unigoa.ac.in>
2021-03-22 17:55:42 +0530

    added carousel

--- client/src/components/Carousel.svelte
+++ client/src/components/Carousel.svelte
@@ -1,7 +1,33 @@
-<div>
-    carousel     
-</div>
+<div id="demo" class="carousel slide" data-ride="carousel">
 
+  
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/dog14.jpg" alt="dog1" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="images/dog3.jpg" alt="dog2" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="images/dog10.jpg" alt="dog3" width="1100" height="500">
+    </div>
+  </div>
+  
+  
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
 <style>
     div {
         height: 75vh;
