d3852b6aa76ecf4ee7feeab2d70163fd5fbf5108
git-shru <mca.1904@unigoa.ac.in>
2021-03-29 22:51:09 +0530

    navbar
    (cherry picked from commit 61b58778f93aebc067c97da9af29fcd4d77c89ec)

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,14 +2,68 @@
 	import { Router, Route, Link } from "svelte-routing";
 	import Home from './components/Home.svelte'
 	import Footer from './components/Footer.svelte'
+
+  import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+  } from 'sveltestrap';
+
+  let isOpen = false;
+
+  function handleUpdate(event) {
+    isOpen = event.detail.isOpen;
+  }
+
+
 </script>
 
 <main>
   <Router>
-  <nav class="pt-2 pb-2">
-    nav
-    <!-- @S add nav items (use Link) -->
-  </nav>
+ 
+ <nav>
+
+<Navbar color=" " dark expand="md">
+  <NavbarBrand href="/">Doggo ♡</NavbarBrand>
+  <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+  <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+    <Nav class="ml-auto" navbar>
+      <NavItem>
+        <NavLink href="#Home/">Home</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#Adopt/">Adopt</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#Foster/">Foster</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="Volunteer">Volunteer</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="Event">Event</NavLink>
+      </NavItem>
+      <UncontrolledDropdown nav inNavbar>
+        <DropdownToggle nav caret>Help</DropdownToggle>
+        <DropdownMenu right>
+          <DropdownItem>Pet food</DropdownItem>
+          <DropdownItem>Vaccination Centre</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    </Nav>
+  </Collapse>
+</Navbar>
+
+ </nav>
+
     <!-- add routes -->
     <Route path="/"><Home /></Route>
   </Router>
@@ -24,5 +78,10 @@
 
 	@media (min-width: 640px) {
 		
+	}
+
+	nav{
+	  background-color:#302121;
+
 	}
 </style>
