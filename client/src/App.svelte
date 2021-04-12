bf99427e8a9892d58b789f44acde7cabebe9c6ee
git-shru <mca.1904@unigoa.ac.in>
2021-04-12 19:14:33 +0530

    navbar fix

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,65 +3,13 @@
   import Home from "./components/Home.svelte";
   import Doggos from "./components/Doggos.svelte";
   import Footer from "./components/Footer.svelte";
+  import Navbar from "./components/Navbar.svelte";
 
-  import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-  } from "sveltestrap";
-
-  let isOpen = false;
-
-  function handleUpdate(event) {
-    isOpen = event.detail.isOpen;
-  }
 </script>
 
 <main>
   <Router>
-    <nav class="px-3">
-      <Navbar color=" " dark expand="md">
-        <NavbarBrand href="/">
-          <img src="images/icons/logo-w.png" alt="logo" />
-        </NavbarBrand>
-        <NavbarToggler on:click={() => (isOpen = !isOpen)} />
-        <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
-          <Nav class="ml-auto" navbar>
-            <NavItem>
-              <Link class="nav-link" to="adopt">Adopt</Link>
-            </NavItem>
-            <NavItem>
-              <NavLink>Foster</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="Volunteer">Volunteer</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#Donate/">Donate</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="Event">Events</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>Help</DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Pet food</DropdownItem>
-                <DropdownItem>Vaccination Centre</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </nav>
-
+    <Navbar />
     <Route path="/"><Home /></Route>
     <Route path="adopt"><Doggos /></Route>
     <Footer />
---
+++ client/src/components/Navbar.svelte
@@ -0,0 +1,61 @@
+<script>
+
+import { Link } from "svelte-routing";
+
+ import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+  } from "sveltestrap";
+
+  let isOpen = false;
+
+  function handleUpdate(event) {
+    isOpen = event.detail.isOpen;
+  }
+
+</script>
+
+
+<nav class="px-3">
+      <Navbar color=" " dark expand="md">
+        <NavbarBrand href="/">
+          <img src="images/icons/logo-w.png" alt="logo" />
+        </NavbarBrand>
+        <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+        <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+          <Nav class="ml-auto" navbar>
+            <NavItem>
+              <Link class="nav-link" to="adopt">Adopt</Link>
+            </NavItem>
+            <NavItem>
+              <NavLink>Foster</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="Volunteer">Volunteer</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#Donate/">Donate</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="Event">Events</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>Help</DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Pet food</DropdownItem>
+                <DropdownItem>Vaccination Centre</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </nav>
