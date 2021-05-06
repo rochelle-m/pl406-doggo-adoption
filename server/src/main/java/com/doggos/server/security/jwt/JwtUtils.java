d2cbedcd0ac543561b1633d7c3bba7cad2bf8c20
rochelle-m <70574900+rochelle-m@users.noreply.github.com>
2021-05-06 22:06:26 +0530

    Create JWT Utility class

--- server/src/main/java/com/doggos/server/security/jwt/JwtUtils.java
+++ server/src/main/java/com/doggos/server/security/jwt/JwtUtils.java
@@ -1,8 +1,14 @@
 package com.doggos.server.security.jwt;
 
+import com.doggos.server.security.services.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+
+import java.util.Date;
 
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
@@ -12,5 +18,27 @@ public class JwtUtils {
 
     @Value("${app.jwtExpirationMs}")
     private int jwtExpirationMs;
+
+    public String generateJwtToken(Authentication authentication) {
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
+        return Jwts.builder().setSubject(userPrincipal.getUsername())
+                .setIssuedAt(new Date()).setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret).compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public Boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return false;
+    }
 
 }
