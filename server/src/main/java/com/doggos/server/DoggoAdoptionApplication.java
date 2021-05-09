d6b8dcd79f31a647078a136ed458457a640cfa55
rochelle-m <70574900+rochelle-m@users.noreply.github.com>
2021-05-09 23:55:18 +0530

    Fix: Handle CORS

--- server/src/main/java/com/doggos/server/DoggoAdoptionApplication.java
+++ server/src/main/java/com/doggos/server/DoggoAdoptionApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -23,4 +25,13 @@ public class DoggoAdoptionApplication {
         };
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:5000");
+            }
+        };
+    }
 }
