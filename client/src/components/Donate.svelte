ed7ed476dd18301753c159c53be162f33c534ae0
rochelle-m <70574900+rochelle-m@users.noreply.github.com>
2021-05-19 12:54:14 +0530

    Add donation options

--- client/src/components/Donate.svelte
+++ client/src/components/Donate.svelte
@@ -1,7 +1,7 @@
 <script>
   import Banner from "./Banner.svelte";
 
-   export let title = "Dog Adoption and Care · Donate";
+  export let title = "Dog Adoption and Care · Donate";
 
   let message = "Donate";
   let imgSrc = "images/donate.jpg";
@@ -13,12 +13,33 @@
 
 <div>
   <Banner {message} {imgSrc} />
+
+  <div class="d-flex justify-content-around mt-4 row container" style="margin: auto">
+    <div class="col-sm-4">
+      <div class="card border-dark mb-3">
+        <div class="card-header">
+          <span class="d-inline-block">Donate Items</span>
+          <span class="d-inline-block btn float-right">
+            <i class="fas fa-box-open" />
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="card border-dark mb-3">
+        <div class="card-header">
+          <span class="d-inline-block">Make a monetary donation</span>
+          <span class="d-inline-block btn float-right">
+            <i class="far fa-credit-card" />
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <small> All donations are exempted under the 80g certificate. </small>
-  <h3>⧛ Under construction ⌂ ⧛</h3>
 </div>
 
 <style>
-  h3 {
-    padding: 4em 0;
-  }
+
 </style>
