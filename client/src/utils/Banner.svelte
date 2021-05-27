613f708104ae5ec8ec64b8a89582f483758c1d36
rochelle-m <70574900+rochelle-m@users.noreply.github.com>
2021-05-27 21:55:56 +0530

    Add a breadcrumb

--- client/src/utils/Banner.svelte
+++ client/src/utils/Banner.svelte
@@ -10,20 +10,31 @@
   out:fade
 >
   <img src={imgSrc} alt="banner" />
-  <h2>{message}</h2>
+  <div class="banner-title">
+    <h2>{message}</h2>
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb bg-transparent">
+        <li class="breadcrumb-item"><a href="/" class="text-white">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">{message}</li>
+      </ol>
+    </nav>
+  </div>
 </div>
 
 <style>
-  h2 {
+  .banner-title {
     position: absolute;
     z-index: 10;
-    top: 18vw;
+    top: 15vw;
     text-align: center;
     display: block;
+    color: white;
+  }
+
+  .banner-title h2  {
     text-transform: uppercase;
     padding-bottom: 0.4em;
     border-bottom: 1px solid rgb(255, 254, 254);
-    color: white;
   }
 
   img {
