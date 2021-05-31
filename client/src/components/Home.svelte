5e9fc5b8c7be00b95f097460a1501b41c5c28bc2
rochelle-m <70574900+rochelle-m@users.noreply.github.com>
2021-05-31 13:25:30 +0530

    Add adoption gallery on home page

--- client/src/components/Home.svelte
+++ client/src/components/Home.svelte
@@ -1,10 +1,13 @@
 <script>
   import Carousel from "../utils/Carousel.svelte";
+  
+  import AdoptGallery from "../utils/AdoptGallery.svelte";
 </script>
 
 <div>
   <Carousel />
   <h3>⧛ Under construction ⌂ ⧛</h3>
+  <AdoptGallery/>
 </div>
 
 <style>
