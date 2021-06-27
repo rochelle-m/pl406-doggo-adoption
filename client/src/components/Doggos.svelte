be3cb42c77cbd738061e4e6591ff7628b9f85ed9
rochelle-m <mca.1930@unigoa.ac.in>
2021-06-27 11:25:47 +0530

    Minor fixes
    Replace mounting with a await-then-catch block

--- client/src/components/Doggos.svelte
+++ client/src/components/Doggos.svelte
@@ -20,15 +20,26 @@
       setTimeout(() => {
         show = true;
       }, 1500);
-    } catch (err) {
-      error = err.message;
+    } catch (_) {
+      error = "Some error occured";
     }
   });
 
   let message = "Adopt";
   let imgSrc = "images/dog11.jpg";
-
 </script>
+
+<style>
+  .addbtn {
+    height: 40px;
+    padding: 3px;
+    background-color: grey;
+    color: white;
+    font-size: 15px;
+    border-radius: 4px;
+    border: 1px solid darkgrey;
+  }
+</style>
 
 <svelte:head>
   <title>{title}</title>
@@ -36,8 +47,8 @@
 
 <div>
   <Banner {message} {imgSrc} />
-  <div class="d-flex flex-wrap justify-content-around spacing">
-      {#each doggos as doggo}
+  <div class="d-flex flex-wrap justify-content-around my-4">
+    {#each doggos as doggo}
       {#if !doggo.isAdopted && !doggo.isFostered}
         <Doggo src={doggo.primaryImg} {doggo}>
           <p slot="name">{doggo.name}</p>
@@ -53,21 +64,4 @@
 
 <BeforeYouAdopt {show} />
 
-<a class="addbtn" href="post" ><b>Add Post</b></a>
-
-<style>
- 
-  .spacing {
-    margin: 40px 0px;
-  }
-
-  .addbtn{
-  height:40px;
-  padding:3px;
-  background-color:grey;
-  color:white;
-  font-size:15px;
-  border-radius:4px;
-  border:1px solid darkgrey;
-}
-</style>
+<a class="addbtn" href="post"><b>Add Post</b></a>
